ea33d61abf3190f04724bd6e5a3bf25055cee6a3
guilhermeaeme <guilhermeaeme@gmail.com>
2019-04-06 13:23:43 -0300

    home products filter

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const Product = require('../models/product');
 
 
 //Index
 
-router.get('/', (req,res,next) =>{
-    res.render('index');
+router.get('/', (req, res, next) => {
+    Product.find().populate('owner').sort({ createdAt: -1 })
+        .then(products => {
+            Product.find().distinct('category')
+                .then(categories => {
+                    res.render('index', {
+                        products, categories
+                    });
+                })
+                .catch(err => {
+                    throw new Error(err);
+                });
+        })
+        .catch(err => {
+            throw new Error(err);
+        });
 });
-
 
 module.exports = router;
